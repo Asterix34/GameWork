59a634678136651bb405e5c8c98e5b33e25ab8f1
Asterix34 <bclerc74@gmail.com>
2014-12-07 00:49:53 +0100

    Painting Components Handling Escape to close

--- src/com/hb/td/graphic/Screen.java
+++ src/com/hb/td/graphic/Screen.java
@@ -1,28 +1,51 @@
 package com.hb.td.graphic;
 
+import java.awt.Color;
 import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
+import com.hb.td.input.KeyHandler;
+
 public class Screen extends JPanel implements Runnable {
 	// create a new thread to run the screen (which is runnable)
 	Thread thread = new Thread(this);
-	
 	Frame frame;
 	
-	private int fps = 0;
+	private int fps = 0;	
+	public boolean running = false;
+	public int scene = 0;
+	
 	
 	public Screen(Frame frame) {
+		this.frame = frame;
+		
+		this.frame.addKeyListener(new KeyHandler(this));
+		
 		// start thread (launch run);
 		thread.start();
-		this.frame = frame;
 	}
 	
 	@Override
 	public void paintComponent(Graphics g) {
 		g.clearRect(0, 0, this.frame.getWidth(), this.frame.getHeight());
+
 		
+		if (scene == 0) {
+			g.setColor(Color.BLUE);
+		} else if (scene == 1) {
+			g.setColor(Color.GREEN);
+		} else {
+			g.setColor(Color.WHITE);
+		}
+		g.fillRect(0,0, this.frame.getWidth(), this.frame.getHeight());
+		
+		
+		g.setColor(Color.WHITE);
 		g.drawString(fps + "", 10, 10);
+		
+		// draw fps last to overlap
+
 	}; 
 
 	@Override
@@ -32,7 +55,11 @@ public class Screen extends JPanel implements Runnable {
 		long lastFrameTime = System.currentTimeMillis();
 		int nbFrames = 0;
 		// loop on the game
-		while(true) {
+		
+		running = true;
+		scene = 0;
+		
+		while(running) {
 			repaint();
 			
 			nbFrames++;
@@ -50,6 +77,28 @@ public class Screen extends JPanel implements Runnable {
 				e.printStackTrace();
 			}
 		}
-	}
+		
+		System.exit(0);
+		
 
+	}
+	
+	private void stopGame() {
+		// TODO Auto-generated method stub
+		running = false;
+	}
+	
+	public class KeyTyped {
+		public void keyESC() {
+			// TODO Auto-generated method stub
+			stopGame();
+		}
+
+		public void keySPACE() {
+			// TODO Auto-generated method stub
+			scene = 1;
+		}
+
+
+	}
 }
---
+++ src/com/hb/td/input/KeyHandler.java
@@ -0,0 +1,48 @@
+package com.hb.td.input;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import com.hb.td.graphic.Screen;
+
+public class KeyHandler implements KeyListener{
+	
+	private Screen screen;
+	private Screen.KeyTyped keyTyped;
+	
+	public KeyHandler(Screen screen) {
+		this.screen = screen;
+		
+		// this.is = weird !
+		this.keyTyped = this.screen.new KeyTyped();
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// every time a key is pressed
+		int keyCode = e.getKeyCode();
+		System.out.println(keyCode);
+		if (keyCode == e.VK_ESCAPE) {
+			this.keyTyped.keyESC();
+		}
+		if (keyCode == e.VK_SPACE) {
+			this.keyTyped.keySPACE();
+		}
+			
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
